337a9b50d22ad5824c4c74343c76ba1eb6e2b213
LUXOFT\MElsaka <melsaka@luxoft.com>
2023-08-08 12:59:00 +0300

    add welcome page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MapComponent } from './map/map.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/welcome', pathMatch: 'full' },
+  { path: 'welcome', component: WelcomeComponent },
+  { path: 'map', component: MapComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
